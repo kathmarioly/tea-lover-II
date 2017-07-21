7ed2b922e186a335b148f7e5053e4e058c404183
kathmarioly <katerinemarioly@gmail.com>
2017-07-21 00:16:39 -0400

    JavaScript: first item completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,14 @@
-var susNewsletter =document.getElementById("suscribirse");
+var susNewsletter = document.getElementById("suscribete");
 
-susNewsletter.addEventListener("click" function(){
+susNewsletter.addEventListener("click", function(){
+
 	var nombre = document.getElementById("name").value;
 	var correo = document.getElementById('email').value;
 	var direccion = document.getElementById("address").value;
+	var tipos = document.getElementById("lista-te").value;
 
 	if (nombre.charAt(0) != nombre.charAt(0).toUpperCase() || nombre.length == 0 || nombre == ""){
-		alert("NOMBRE no valido /n Intentalo nuevamente"); 
+		alert("¡NOMBRE no valido! \n(La primera letra debe ir en Mayuscula)"); 
 	}
 
 });
